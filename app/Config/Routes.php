a3e6536609841ea40c664bbb37cbcc213608369f
finkastwn <winafinkastwn@gmail.com>
2025-05-19 17:01:57 +0700

    fix: remove test-session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,5 +9,3 @@ $routes->get('/', 'Dashboard::index', ['filter' => 'auth']);
 $routes->get('/login', 'Auth::login');
 $routes->post('/login', 'Auth::doLogin');
 $routes->get('/logout', 'Auth::logout', ['filter' => 'auth']);
-$routes->get('/test-session', 'TestSession::index');
-
