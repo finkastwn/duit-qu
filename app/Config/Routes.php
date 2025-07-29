f4d14c85e36a01ceeb6a6790f75874bfd290bcb6
Wina <winafinkastwn@gmail.com>
2025-07-29 21:26:49 +0700

    feat: reset password

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,5 @@ $routes->get('/', 'Dashboard::index', ['filter' => 'auth']);
 $routes->get('/login', 'Auth::login');
 $routes->post('/login', 'Auth::doLogin');
 $routes->get('/logout', 'Auth::logout', ['filter' => 'auth']);
+$routes->get('/forgot-password', 'Auth::forgotPassword');
+$routes->post('/forgot-password', 'Auth::doForgotPassword');
